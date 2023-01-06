4061cc9fc01151f5052584b71ec7e5b3dcac367d
jgenc <jorgen.cani7@outlook.com>
2023-01-06 13:49:33 +0200

    feat: initialized project

---
+++ src/main/java/gr/hua/dit/distributed/project_46/Project46Application.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.distributed.project_46;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project46Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project46Application.class, args);
+    }
+
+}
---
+++ src/test/java/gr/hua/dit/distributed/project_46/Project46ApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.distributed.project_46;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project46ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
