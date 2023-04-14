b4f637639f3638af99b153c624a347c3eb35e643
jgenc <jgen.cani@gmail.com>
2023-04-14 19:40:22 +0300

    feat: .env and various files

--- app/db/database.py
+++ app/db/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# TODO: Change me when deploying
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:dev@localhost/db"
+from ..settings import get_settings
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URI = get_settings().SQLALCHEMY_DATABASE_URI
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
