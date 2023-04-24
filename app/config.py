2f02d5f731ca22160a63ff141428b3730816b2a6
jgenc <jgen.cani@gmail.com>
2023-04-25 00:22:24 +0300

    fix: removed unnecessary core files

--- app/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URI: str = "NOTHING"
-
-    class Config:
-        env_file = ".env"
--- app/settings.py
+++
@@ -1,8 +0,0 @@
-from functools import lru_cache
-
-from . import config
-
-
-@lru_cache()
-def get_settings():
-    return config.Settings()
